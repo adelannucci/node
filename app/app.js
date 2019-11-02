8b9ce9a8a0cd002c7f60ca62207c0ad82d7f2f45
Andre <adelannucci@MacBook-Air-de-Hudimila.local>
2019-11-01 23:02:00 -0300

    hbs layout

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 const app = express();
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.engine(
+    'hbs',
+    expressHbs({
+      layoutsDir: 'views/layouts/',
+      defaultLayout: 'main-layout',
+      extname: 'hbs'
+    })
+  );
+  app.set('view engine', 'hbs');
+  app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
